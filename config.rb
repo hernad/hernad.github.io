e5f538f607b7ff55a2f54ebf05ae3f8806299411
Ernad Husremovic <hernad@bring.out.ba>
2015-02-20 17:07:29 +0100

    init

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/Users/hernad/blog/grain-theme-octopress/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/Users/hernad/blog/grain-theme-octopress/theme/sass"
+generated_images_path = "/Users/hernad/blog/grain-theme-octopress/.cache/compass/images"
+images_path = "/Users/hernad/blog/grain-theme-octopress/theme/images"
+fonts_path = "/Users/hernad/blog/grain-theme-octopress/theme/fonts"
+
+line_comments = false
+output_style = :compressed
